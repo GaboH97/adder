332d72c00e88cbf9ed59dd6357922ef48e3a0560
Gabriel Huertas <gaahuertas@gmail.com>
2025-01-04 13:18:14 -0500

    adds documentation

--- add.go
+++ add.go
@@ -1,5 +1,8 @@
+// Package adder includes a math function to add two integers
 package adder
 
+// Add takes in two numbers and returns the result.
+// To learn more about maths check this [site]:https://www.mathisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
